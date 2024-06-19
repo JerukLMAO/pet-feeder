c91341dcee0bbc908666f0cec67f601cf0ff89dd
JerukLMAO <121074155+JerukLMAO@users.noreply.github.com>
2024-06-19 11:26:24 +0700

    Fix bug

--- src/main.cpp
+++ src/main.cpp
@@ -18,41 +18,64 @@ String pass = "fadhligibran12"; // Your Wi-Fi network password
 String token = "6526441782:AAFJM0zrJW1GmXOcQazzifJE3h-oxG7k8Hg"; // Your Telegram bot token
 const int id = 1221284266;                                       // Telegram chat ID where messages will be sent
 
-unsigned long interval = 21600; // Feeding interval in seconds (6 hours)
-int prevEpoch = 0;           
+unsigned long interval = 86400; // Feeding interval in seconds (24 hours)
+int prevEpoch = 0;
 
-volatile int hasFedFish = 0;            // Counts the number of times the fish have been fed
-volatile boolean fishFed = false;   
-volatile boolean buttonPressed = false; 
-volatile int servoLastVal =90;
+volatile int hasFedFish = 0; // Counts the number of times the fish have been fed
+volatile boolean fishFed = false;
+volatile boolean buttonPressed = false;
+volatile int timesPressed = 0;
+volatile int servoPos = 90;
+volatile boolean servoDirection = 1; // 1 = Forward   0 = Backward
 
-NTPClient timeClient(ntpUDP, "ntp.bmkg.go.id", 25200, 30000); 
+NTPClient timeClient(ntpUDP, "ntp.bmkg.go.id", 25200, 30000);
 
 void feedFish()
 {
-  servoLastVal = myServo.read()
-  myServo.write(60);          
-  delayMicroseconds(500000); 
-  myServo.write(90);         
+  if (myServo.read() == 0 || myServo.read() == 90)
+  {
+    servoDirection = 1; // Forward
+  }
+  else if (myServo.read() == 180)
+  {
+    servoDirection = 0; // Backward
+  }
+
+  if (servoDirection)
+  {
+    servoPos = myServo.read() + 10;
+  }
+  else
+  {
+    servoPos = myServo.read() - 10;
+  }
+
+  constrain(servoPos, 0, 180);
+  myServo.write(servoPos);
 
   hasFedFish++;
-
   fishFed = true;
 }
 
 // Interrupt Service Routine (ISR) for fish button press
 IRAM_ATTR void fish()
 {
-  buttonPressed = true;
-  feedFish();
+  timesPressed++;
+  delayMicroseconds(20e4);
+  if (timesPressed >= 3)
+  {
+    buttonPressed = true;
+    feedFish();
+    timesPressed = 0;
+  }
 }
 
 void setup()
 {
-  myServo.attach(4, 500, 2500);
+  myServo.attach(5, 500, 2500);
 
-  pinMode(12, INPUT_PULLUP);
-  attachInterrupt(12, fish, FALLING);
+  pinMode(14, INPUT_PULLUP);
+  attachInterrupt(14, fish, FALLING);
 
   myBot.wifiConnect(ssid, pass);
 
@@ -66,7 +89,7 @@ void setup()
   // Send a startup message to Telegram chat if connection is successful
   if (myBot.testConnection())
   {
-    myBot.sendMessage(id, "Bot dinyalakan, terhubung ke " + ssid + ", IP: " + ipToString(WiFi.localIP()));
+    myBot.sendMessage(id, "Bot dinyalakan, terhubung ke " + ssid);
   }
 
   ArduinoOTA.begin();
@@ -79,10 +102,6 @@ void loop()
   ArduinoOTA.handle();
 
   // Send Telegram messages for feeding events and reset flags
-  {
-    myBot.sendMessage(id, "Kucing telah diberi makan");
-    catFed = false;
-  }
   if (fishFed)
   {
     myBot.sendMessage(id, "Ikan telah diberi makan");
@@ -97,24 +116,28 @@ void loop()
   // Check for scheduled feeding time based on interval
   if (timeClient.getEpochTime() - prevEpoch >= interval)
   {
-    prevEpoch = timeClient.getEpochTime();      // Update previous feeding time
-    feedFish();                                 // Trigger fish feeding
+    prevEpoch = timeClient.getEpochTime();   // Update previous feeding time
+    feedFish();                              // Trigger fish feeding
     myBot.sendMessage(id, "waktunya makan"); // Send feeding notification
   }
 
   // Check for new Telegram messages and handle commands
   if (myBot.getNewMessage(msg))
   {
-    switch (msg.text)
+    if (msg.text == "Feed fish")
     {
-    case "Feed fish":
       feedFish();
-      break;
-    case "Reset":
+    }
+
+    if (msg.text == "Reset jadwal")
+    {
       prevEpoch = timeClient.getEpochTime();
       myBot.sendMessage(id, "Telah di reset");
-      break;
-    case "Next feed":
+    }
+
+    // Calculate and send time remaining for next scheduled feeding
+    if (msg.text == "Next feed")
+    {
       int t = interval - (timeClient.getEpochTime() - prevEpoch);
       int s = t % 60;
       t = (t - s) / 60;
@@ -122,16 +145,18 @@ void loop()
       t = (t - m) / 60;
       int h = t;
       myBot.sendMessage(id, "Dijadwalkan diberi makan dalam " + String(h) + ":" + String(m) + ":" + String(s));
-      break;
-    case "Info":
-      myBot.sendMessage(id, "Telah memberi makan kucing " + String(hasFedCat) + " kali, dan ikan " + String(hasFedFish) + " kali. NTP: " + timeClient.getFormattedTime());
-      break;
-    case "Update ntp":
+    }
+
+    // Send status information (feeding counts and NTP time)
+    if (msg.text == "Info")
+    {
+      myBot.sendMessage(id, "Telah memberi makan ikan " + String(hasFedFish) + " kali. NTP: " + timeClient.getFormattedTime());
+    }
+
+    if (msg.text == "Update ntp")
+    {
       myBot.sendMessage(id, "Mengupdate NTP...");
       timeClient.forceUpdate();
-      break;
-    default:
-      myBot.sendMessage(id, "Perintah tidak dikenal");
     }
   }
 }
